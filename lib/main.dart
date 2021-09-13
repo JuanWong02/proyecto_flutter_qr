86b560301a12fd2817012b8534320382c2f6a8a5
JuanWong02 <juanwong0211@gmail.com>
2021-09-13 12:29:20 -0600

    pantalla inicio

---
+++ lib/main.dart
@@ -0,0 +1,63 @@
+import 'package:flutter/material.dart';
+import 'package:flutter/rendering.dart';
+
+void main() => runApp(MyApp());
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: 'ESCANER Y GENERADOR QR',
+      theme: ThemeData(
+        primaryColor: Color.fromRGBO(5, 145, 110, 1),
+        cardColor: Theme.of(context).primaryColor,
+        accentColor: Colors.white,
+        splashColor: Colors.lightGreen,
+        fontFamily: 'Roboto',
+      ),
+      home: PantallaInicio(),
+    );
+  }
+}
+
+
+class PantallaInicio extends StatelessWidget {
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        centerTitle: true,
+        title: Text('ESCANER Y GENERADOR QR'),
+      ),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          crossAxisAlignment: CrossAxisAlignment.stretch,
+          children: <Widget> [
+            Padding(padding: EdgeInsets.symmetric(vertical: 8.0, horizontal: 16.0),
+            child: ElevatedButton(
+              style: ElevatedButton.styleFrom(
+                primary: Theme.of(context).primaryColor
+              ),
+              onPressed: () {},
+              child: const Text('ESCANEAR QR'),
+            ),
+            ),
+            Padding(padding: EdgeInsets.symmetric(vertical: 8.0, horizontal: 16.0),
+            child: ElevatedButton(
+              style: ElevatedButton.styleFrom(
+                primary: Theme.of(context).primaryColor,
+              ),
+              onPressed: () {},
+              child: const Text('GENERAR QR'),
+            ),
+
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
